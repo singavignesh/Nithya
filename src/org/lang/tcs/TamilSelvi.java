d03b3045cde7f34799673413e00e371b10fb44ab
singavignesh <k.vignesh28@gmail.com>
2020-08-15 23:43:56 +0530

    adding

---
+++ src/org/lang/tcs/TamilSelvi.java
@@ -0,0 +1,15 @@
+package org.lang.tcs;
+
+public class TamilSelvi {
+	
+	public void nithya() {
+		System.out.println("nithya");
+	}
+	public static void main(String[] args) {
+		TamilSelvi tn = new TamilSelvi();
+		tn.nithya();
+		
+	}
+
+
+}
