32eef4852a89de1898c815cf94db3a4b87eeefda
singavignesh <k.vignesh28@gmail.com>
2020-08-16 14:34:21 +0530

    vicky added

--- src/org/lang/tcs/Sample.java
+++ src/org/lang/tcs/Sample.java
@@ -2,6 +2,10 @@ package org.lang.tcs;
 
 public class Sample {
 
+	public void getdata()
+	{
+		System.out.println("gtdata");
+	}
 	public static void main(String[] args) {
 		System.out.println("Sample");
 
